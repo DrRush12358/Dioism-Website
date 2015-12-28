24a1c36f53bae08e0bf61342f9b7ff23bc90a7ed
James <James@James_Laptop3>
2015-12-27 23:02:58 -0500

    Mid way back up

---
+++ public_html/JS/navbar_load.js
@@ -0,0 +1,10 @@
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+
+$('document').ready(function(){
+    $('nav').text("<div class='container-fluid'><!-- Brand and toggle get grouped for better mobile display --><div class='navbar-header'><button type='button' class='navbar-toggle collapsed' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1' aria-expanded='false'><span class='sr-only'>Toggle navigation</span><span class='icon-bar'></span><span class='icon-bar'></span><span class='icon-bar'></span></button><a class='navbar-brand' href='/Book%20of%20Dio%20Website/index.html'><img width='50' height='50' src='/Book%20of%20Dio%20Website/Images/Navbox-head.jpg' /></a></div><!-- Collect the nav links, forms, and other content for toggling --><div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'><ul class='nav navbar-nav'><li class='active'><a href='/Book%20of%20Dio%20Website/forum/index.php'>Forum<span class='sr-only'>(current)</span></a></li><li><a href='http://dioism.wikia.com'>Wiki</a></li><li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Scriptures <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='Book.html'>The Book of Dio</a></li><li><a href='Circles.html'>The Circles of Dio</a></li><li><a href='Trumpets.html'>The Trumpets of Dio</a></li><li><a href='Scripture.html'>The Scripture of Truth</a></li><li><a href='Chronicles.html'>The Chronicles of Dio</a></li></ul></li></ul><ul class='nav navbar-nav navbar-right'><li class='dropdown'><a href='#' class='dropdown-toggle' data-toggle='dropdown' role='button' aria-haspopup='true' aria-expanded='false'>Contact <span class='caret'></span></a><ul class='dropdown-menu'><li><a href='About.html'>About us</a></li><li><a href='Contact.html'>Contact us</a></li></ul></li></ul></div><!-- /.navbar-collapse --></div>");
+});
